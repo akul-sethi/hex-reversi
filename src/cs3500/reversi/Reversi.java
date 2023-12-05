d3e017a2b222a0e96f66c81be9552cc0be48ccd0
Akul Sethi <sethi.ak@northeastern.edu>
2023-12-04 23:56:42 -0500

    Revert "super strat added to main"
    This reverts commit c326709443503421b0b6a0439b58820491327a77.

--- src/cs3500/reversi/Reversi.java
+++ src/cs3500/reversi/Reversi.java
@@ -16,7 +16,6 @@ import cs3500.reversi.strategy.AvoidNextToCornersStrategy;
 import cs3500.reversi.strategy.CaptureCornersStrategy;
 import cs3500.reversi.strategy.CaptureMaxStrategy;
 import cs3500.reversi.strategy.MiniMaxStrategy;
-import cs3500.reversi.strategy.TryTwo;
 import cs3500.reversi.view.GUIReversiView;
 import cs3500.reversi.view.ReversiView;
 
@@ -52,9 +51,6 @@ public final class Reversi {
     playerTypes.put("avoid-corners", (name) -> new MachinePlayer(name,
             new AvoidNextToCornersStrategy()));
     playerTypes.put("minimax", (name) -> new MachinePlayer(name, new MiniMaxStrategy()));
-    playerTypes.put("super", (name) -> new MachinePlayer(name, new TryTwo(new TryTwo(new TryTwo(
-            new AvoidNextToCornersStrategy(), new CaptureMaxStrategy()), new MiniMaxStrategy()),
-            new CaptureCornersStrategy())));
   }
 
   /**
